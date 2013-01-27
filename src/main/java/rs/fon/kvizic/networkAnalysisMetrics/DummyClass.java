74f22342d4da164deaca411cdd6f728378263963
Danilo Pavkov <danilopavkov@yahoo.com>
2013-01-27 01:41:31 +0100

    cleaning up

--- src/main/java/rs/fon/kvizic/networkAnalysisMetrics/DummyClass.java
+++ src/main/java/rs/fon/kvizic/networkAnalysisMetrics/DummyClass.java
@@ -1,9 +1,10 @@
+package rs.fon.kvizic.networkAnalysisMetrics;
 
 /**
  * necessary in order to create javadoc and bypass oss checking
  * @author danilo
  *
  */
-public class DummyClass {
+class DummyClass {
 
 }
